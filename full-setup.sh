6cacb589771389e7b34edbbf3e0a59d4640baa73
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-13 22:12:50 +0530

    updated files

--- full-setup.sh
+++ full-setup.sh
@@ -1,5 +1,5 @@
 for component in frontend cart catalogue payment user shipping redis mysql mongodb rabbitmq ; do
 
-   ansible-playbook -i  ${component}.devopsb26.online roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME={component} -e env=dev
+   ansible-playbook -i  ${component}-dev.devopsb26.online roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME={component} -e env=dev
 
  done  
